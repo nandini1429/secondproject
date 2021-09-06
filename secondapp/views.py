f79f5e811ebb814c706636d1608d2fc7b4167aa2
nandini1429 <umnreddy314@gmail.com>
2021-09-06 10:17:37 +0530

    second commit

---
+++ secondapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def good_morning_view(request):
+    msg = "<h1>Hello Friend Good Morning!!!</h1>"
+    return HttpResponse(msg)
+
+def good_afternoon_view(request):
+    msg = "<h1>Hello Friend Good Afternoon!!!</h1>"
+    return HttpResponse(msg)
+
+def good_evening_view(request):
+    msg = "<h1>Hello Friend Good Evening!!!</h1>"
+    return HttpResponse(msg)
